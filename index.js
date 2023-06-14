e5d2c1dc3f08c9df65f5460eb5a0a7af6e5efcfe
scottzyang <scottzyang@gmail.com>
2023-06-14 16:19:16 -0500

    Complete challenge 2

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  io.emit("user connected", "A user has joined the room.")
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit("user disconnected", 'A user has left the room.')
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
